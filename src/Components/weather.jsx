11423a7b1198ab1de45f02778431e3d6addb6443
moeinparvizi <giventulip@gmail.com>
2023-11-03 02:40:14 +0330

    create regex for input

--- src/Components/weather.jsx
+++ src/Components/weather.jsx
@@ -1,9 +1,22 @@
 export default function Weather({ image }) {
   const apikey = "303e94191e8dc309c73f3faba0936d35";
 
-  function search() {
-    const inp = document.querySelector('.top-bar-getcity').value
-    alert(inp)
+  function search(){
+    alert('every thinks is ok')
+  }
+
+  function regTest() {
+    const inp = document.querySelector(".top-bar-getcity").value;
+    const reg = /^[a-z0-9_-]{3,15}$/
+    if (inp == "" || inp == null) {
+      alert("Please enter a city name");
+    } else {
+      if (reg.test(inp)) {
+        search()
+      }else{
+        alert('dorost vared kon')
+      }
+    }
   }
 
   return (
@@ -17,7 +30,7 @@ export default function Weather({ image }) {
       {/*
        !maby its not working role=button
       */}
-      <figure onClick={search} className="top-bar-search" role="button">
+      <figure onClick={regTest} className="top-bar-search" role="button">
         <img src={image} alt="search button" className="search-image" />
       </figure>
     </div>
