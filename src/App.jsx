c9ad17d86e10c86d3740a72ca10a18b61efbf125
moeinparvizi <giventulip@gmail.com>
2023-11-02 21:06:38 +0330

    add images and ready for start project weather

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+import './assets/style/master.css'
+
+// import images
+import searchImage from './assets/images/search.png'
+import clearImage from './assets/images/clear.png'
+import windImage from './assets/images/wind.png'
+import snowImage from './assets/images/snow.png'
+import drizzleImage from './assets/images/drizzle.png'
+import humidityImage from './assets/images/humidity.png'
+import rainImage from './assets/images/rain.png'
+import cloudImage from './assets/images/cloud.png'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
